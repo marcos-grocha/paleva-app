837d4ca81cf4d670cc392f914638faa2d8cd8b13
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-23 02:22:25 -0300

    Test: user sign up

--- app/controllers/pa_leva_controller.rb
+++ app/controllers/pa_leva_controller.rb
@@ -1,3 +1,9 @@
 class PaLevaController < ApplicationController
-  def index; end
+  def index
+    if restaurant_owner_signed_in?
+      'está logado! redirecione pra la'
+    else
+      redirect_to new_restaurant_owner_session_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  
   # Defines my routes
   root "pa_leva#index"
+  devise_for :restaurant_owners
 end
